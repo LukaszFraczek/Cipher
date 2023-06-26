73c3b36320635654d5cb9e5fbc45616d54c310d8
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-06-26 19:17:55 +0200

    feat(CipherData): can represent dataclass as dict

--- cipher_data.py
+++ cipher_data.py
@@ -1,21 +1,23 @@
-from enum import Enum
-from enum import auto
+from enum import Enum, auto
 from dataclasses import dataclass
 
 
-class Rot(Enum):
-    rot13 = auto()
-    rot47 = auto()
+class Rot(str, Enum):
+    ROT13 = 'ROT13'
+    ROT47 = 'ROT47'
 
 
-class Status(Enum):
-    encrypted = True
-    decrypted = False
+class Status(str, Enum):
+    ENCRYPTED = 'ENCRYPTED'
+    DECRYPTED = 'DECRYPTED'
 
 
 @dataclass
 class CipherData:
-    name: str
     text: str
     rot_type: Rot
     status: Status
+
+    def to_dict(self):
+        _dict = self.__dict__.copy()
+        return _dict
