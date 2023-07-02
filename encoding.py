bb67b6e06410a065c188f0fae9233860af76968e
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-03 01:40:24 +0200

    refactor: RotEncryption base class added

--- encoding.py
+++ encoding.py
@@ -4,15 +4,12 @@ from message import Message
 from constants import RotType, Status
 
 
-class Rot13:
-    __LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    __LETTERS_ROT13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+class RotEncryption:
+    _ROT_TYPE: RotType = RotType.NONE
 
     @classmethod
-    def __translate_rot13(cls, text: str) -> str:
-        trans_table = str.maketrans(cls.__LETTERS, cls.__LETTERS_ROT13)
-        output = text.translate(trans_table)
-        return output
+    def _translate(cls, text: str) -> str:
+        return text
 
     @classmethod
     def encrypt(cls, data: Message) -> Message:
@@ -22,10 +19,10 @@ class Rot13:
         #   pass  # Raise wrong encryption type error
 
         result = copy(data)
-        result.text = cls.__translate_rot13(data.text)
-        result.rot_type = RotType.ROT13
+        result.text = cls._translate(data.text)
+        result.rot_type = cls._ROT_TYPE
         result.status = Status.ENCRYPTED
-        return data
+        return result
 
     @classmethod
     def decrypt(cls, data: Message) -> Message:
@@ -35,15 +32,29 @@ class Rot13:
         #   pass  # Raise wrong encryption type error
 
         result = copy(data)
-        result.text = cls.__translate_rot13(data.text)
+        result.text = cls._translate(data.text)
         result.rot_type = RotType.NONE
         result.status = Status.DECRYPTED
-        return data
+        return result
 
 
-class Rot47:
+class Rot13(RotEncryption):
+    _LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    _LETTERS_ROT13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    _ROT_TYPE = RotType.ROT13
+
     @classmethod
-    def __translate(cls, text: str) -> str:
+    def _translate(cls, text: str) -> str:
+        trans_table = str.maketrans(cls._LETTERS, cls._LETTERS_ROT13)
+        output = text.translate(trans_table)
+        return output
+
+
+class Rot47(RotEncryption):
+    _ROT_TYPE = RotType.ROT47
+
+    @classmethod
+    def _translate(cls, text: str) -> str:
         output = ''
         for letter in text:
             letter_ucp = ord(letter)
@@ -53,30 +64,3 @@ class Rot47:
                 letter_ucp -= 47
             output += chr(letter_ucp)
         return output
-
-    @classmethod
-    def encrypt(cls, data: Message) -> Message:
-        if data.status == Status.ENCRYPTED:
-            pass  # Raise encrypted error
-        # if data.rot_type != RotType.NONE:
-        #   pass  # Raise wrong encryption type error
-
-        result = copy(data)
-        result.text = cls.__translate(data.text)
-        result.rot_type = RotType.ROT47
-        result.status = Status.ENCRYPTED
-        return data
-
-    @classmethod
-    def decrypt(cls, data: Message) -> Message:
-        if data.status == Status.DECRYPTED:
-            pass  # Raise encrypted error
-        # if data.rot_type != RotType.NONE:
-        #   pass  # Raise wrong encryption type error
-
-        result = copy(data)
-        result.text = cls.__translate(data.text)
-        result.rot_type = RotType.NONE
-        result.status = Status.DECRYPTED
-        return data
-
