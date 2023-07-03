a36ba1a4ce9e8df75ca7be26918e0b72db9553df
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-03 17:19:26 +0200

    feat!: Menu can be user-defined now BREAKING CHANGE: Breaks old functionality

--- menu.py
+++ menu.py
@@ -1,18 +1,35 @@
+from typing import Callable
+from dataclasses import dataclass
+
+
+@dataclass
+class MenuItem:
+    option: str
+    description: str
+    function: Callable
+
 
 class Menu:
-    ALLOWED_INPUT = ('1', '2', '3', '4', '9')
+    def __init__(self, *items: MenuItem):
+        self._ALLOWED_INPUTS = tuple([item.option for item in items])
+        self._MENU_ITEMS = tuple([item for item in items])
+        self._MSG_INVALID = "Invalid selection!"
 
-    @staticmethod
-    def show_main_menu() -> None:
-        print('1. Read from file')
-        print('2. Write to file')
-        print('3. Encode')
-        print('4. Decode')
-        print('9. Exit')
+    def show(self) -> None:
+        for item in self._MENU_ITEMS:
+            print(f'{item.option}. {item.description}')
 
-    @staticmethod
-    def user_input() -> str:
+    def _user_input(self) -> int:
         while True:
             user_input = input()
-            if user_input in Menu.ALLOWED_INPUT:
-                return user_input
+            try:
+                index = self._ALLOWED_INPUTS.index(user_input)
+            except ValueError:
+                print(self._MSG_INVALID)
+                continue
+            break
+        return index
+
+    def choice(self) -> None:
+        self._MENU_ITEMS[self._user_input()].function()
+
