5ecdc595d109683e591cf8cc61a1aa5fc14b5e4e
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-04 15:03:56 +0200

    feat: buffer class added

---
+++ buffer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from message import Message
+
+
+class Buffer:
+    def __init__(self):
+        self._memory: List[Message] = []
+
+    def display_all(self):
+        for idx, msg in enumerate(self._memory, 1):
+            print(f'{idx}. Status: {msg.status}, Encryption: {msg.rot_type}')
+            print(msg.text)
+
+    def add(self, msg: Message) -> None:
+        self._memory.append(msg)
+
+    def remove(self, idx_to_dlt) -> None:
+        del self._memory[idx_to_dlt - 1]  # try for IndexError
