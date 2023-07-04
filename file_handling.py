74a5b242939168f42e4c128f9b4786bef767f7b5
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-04 17:28:26 +0200

    feat(FileHandler)!: rewritten save and read methods BREAKING CHANGE: methods now accept buffer-type objects instead of message-type

--- file_handling.py
+++ file_handling.py
@@ -1,21 +1,32 @@
 import json
+from os import path
+from typing import List
 
 from message import Message
-from constants import RotType, Status
+from buffer import MessageBuffer
 
 
 class FileHandler:
     @staticmethod
-    def save_to_json(data: Message, filepath: str) -> bool:
+    def save_to_json(msg_buffer: MessageBuffer, filepath: str) -> None:
+        msg_list: List[Message] = []
+        if path.isfile(filepath):  # File found -> first read from existing file
+            with open(filepath) as file:
+                msg_list = json.load(file)
+
+        for msg in msg_buffer.memory:
+            msg_list.append(msg.to_dict())
+
         with open(filepath, 'w') as file:
-            json.dump(data.to_dict(), file)
-        return True
+            json.dump(msg_list, file, indent=4, separators=(',', ': '))
 
     @staticmethod
-    def read_from_json(filepath: str) -> Message:
+    def read_from_json(filepath: str) -> MessageBuffer:
         with open(filepath) as file:
-            data_dict = json.load(file)
-        data_dict['rot_type'] = RotType(data_dict['rot_type'])
-        data_dict['status'] = Status(data_dict['status'])
-        data_cipher = Message(**data_dict)
-        return data_cipher
+            msg_list: List[Message] = json.load(file)
+
+        buffer = MessageBuffer()
+        for msg in msg_list:
+            buffer.add(msg)
+
+        return buffer
