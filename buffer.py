fdbe364e069d0629ca9bd7ed1a710aa3a65baaf3
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-04 16:38:08 +0200

    refactor!: renamed Buffer to MessageBuffer

--- buffer.py
+++ buffer.py
@@ -3,20 +3,21 @@ from typing import List
 from message import Message
 
 
-class Buffer:
+class MessageBuffer:
     def __init__(self):
-        self._memory: List[Message] = []
+        self.memory: List[Message] = []
 
     def __len__(self):
-        return len(self._memory)
+        return len(self.memory)
 
     def display_all(self):
-        for idx, msg in enumerate(self._memory, 1):
+        for idx, msg in enumerate(self.memory, 1):
             print(f'{idx}. Status: {msg.status}, Encryption: {msg.rot_type}')
             print(msg.text)
 
     def add(self, msg: Message) -> None:
-        self._memory.append(msg)
+        self.memory.append(msg)
 
     def remove(self, idx_to_dlt) -> None:
-        del self._memory[idx_to_dlt - 1]  # try for IndexError
+        del self.memory[idx_to_dlt - 1]  # try for IndexError
+
