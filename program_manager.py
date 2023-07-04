45e6189218b7cb435900aa6a8ba10b242090ebd6
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-04 18:21:38 +0200

    feat(program manager): program manager now uses buffer to store messages

--- program_manager.py
+++ program_manager.py
@@ -1,6 +1,5 @@
-from typing import Union
-
-from menu import Menu, MenuItem, Dialog, DialogItem
+from buffer import MessageBuffer
+from menu import Menu, MenuItem, Dialog, DialogItem, MenuMessages
 from message import Message
 from file_handling import FileHandler
 from encoding import Rot13, Rot47
@@ -10,7 +9,7 @@ from constants import RotType, Status
 class Manager:
     def __init__(self):
         self.__running = True
-        self.buffer: Union[None, Message] = None
+        self.buffer = MessageBuffer()
 
         # create a main menu
         self.main_menu = Menu(
@@ -32,58 +31,88 @@ class Manager:
         )
 
         # create a read file dialog
-        self.read_dialog = Dialog(
-            'Overwrite data in buffer?',
+        self.save_dialog = Dialog(
+            'Save all messages?',
             DialogItem('Y', lambda: True),
             DialogItem('N', lambda: False),
         )
 
     def read_from_file(self) -> None:
-        if self.buffer:
-            self.read_dialog.display()
-            if not self.read_dialog.select():
-                return
         file_path = input('Input path of file to read:\n')
-        self.buffer = FileHandler.read_from_json(file_path)
+        file_msg_buffer = FileHandler.read_from_json(file_path)
+
+        for file_msg in file_msg_buffer.memory:
+            self.buffer.add(file_msg)
 
     def save_to_file(self) -> None:
-        file_path = input('Input path of new file:\n')
-        FileHandler.save_to_json(self.buffer, file_path)
+        self.save_dialog.display()
+        if self.save_dialog.select():
+            # Save all messages
+            file_path = input(MenuMessages.MSG_SAVE_PATH)
+            FileHandler.save_to_json(self.buffer, file_path)
+            return
+
+        # Save single message
+        try:
+            msg_idx = int(input('Input message number to save:\n'))
+            msg_to_save = self.buffer.memory[msg_idx - 1]
+        except ValueError or IndexError:
+            print(MenuMessages.MSG_INVALID_INPUT)
+            return
+
+        # need file path check later
+        file_path = input(MenuMessages.MSG_SAVE_PATH)
+        FileHandler.save_to_json(MessageBuffer(msg_to_save), file_path)
 
     def decode_message(self) -> None:
-        rot_type = self.buffer.rot_type
-        if rot_type == RotType.ROT13:
-            self.buffer = Rot13.decrypt(self.buffer)
-        elif rot_type == RotType.ROT47:
-            self.buffer = Rot47.decrypt(self.buffer)
-        elif rot_type == RotType.NONE:
+        try:
+            msg_idx = int(input('Input message number to decode:\n'))
+            msg_to_decode = self.buffer.memory[msg_idx - 1]
+        except ValueError or IndexError:
+            print(MenuMessages.MSG_INVALID_INPUT)
+            return
+
+        rot = msg_to_decode.rot_type
+        if rot == RotType.ROT13:
+            self.buffer.memory[msg_idx - 1] = Rot13.decrypt(msg_to_decode)
+        elif rot == RotType.ROT47:
+            self.buffer.memory[msg_idx - 1] = Rot47.decrypt(msg_to_decode)
+        elif rot == RotType.NONE:
             print("Message not encrypted!")
             return
         else:
+            # create new exception for this
             raise Exception
-        print(f'Message decrypted successfully from {rot_type}.')
+        print(f'Message decrypted successfully from {rot}.')
 
     def encode_message(self) -> None:
-        self.encode_message_menu.display()
-        rot_type = self.encode_message_menu.select()
+        try:
+            msg_idx = int(input('Input message number to encode:\n'))
+            msg_to_encode = self.buffer.memory[msg_idx - 1]
+        except ValueError:
+            print(MenuMessages.MSG_INVALID_INPUT)
+            return
+        except IndexError:
+            print(MenuMessages.MSG_INVALID_INPUT)
+            return
 
-        if rot_type == RotType.ROT13:
-            self.buffer = Rot13.encrypt(self.buffer)
-        elif rot_type == RotType.ROT47:
-            self.buffer = Rot47.encrypt(self.buffer)
+        self.encode_message_menu.display()
+        rot = self.encode_message_menu.select()
+
+        if rot == RotType.ROT13:
+            self.buffer.memory[msg_idx - 1] = Rot13.encrypt(msg_to_encode)
+        elif rot == RotType.ROT47:
+            self.buffer.memory[msg_idx - 1] = Rot47.encrypt(msg_to_encode)
         else:
+            # create new exception for this
             raise Exception
 
     def new_message(self) -> None:
-        if self.buffer:
-            self.read_dialog.display()
-            if not self.read_dialog.select():
-                return
-        text = input('Input new message:\n')
-        self.buffer = Message(text, RotType.NONE, Status.DECRYPTED)
+        new_msg = input('Input new message:\n')
+        self.buffer.add(Message(new_msg, RotType.NONE, Status.DECRYPTED))
 
     def show_messages(self) -> None:
-        print(self.buffer)
+        self.buffer.display_all()
 
     def run(self):
         while self.__running:
