9f0983fec5e2e4e0a84a5e8c5581aa3d8651347d
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-07 20:52:47 +0200

    refactor(FileHandler)!: read method now returns list of dicts

--- buffer.py
+++ buffer.py
@@ -29,6 +29,6 @@ class MessageBuffer:
     def remove(self, idx_to_dlt) -> None:
         del self.memory[idx_to_dlt]
 
-    def return_dict(self):
+    def to_dict(self):
         return [msg.to_dict for msg in self.memory]
 
--- file_handling.py
+++ file_handling.py
@@ -1,6 +1,6 @@
 import json
 from os import path
-from typing import List
+from typing import List, Dict
 
 from message import Message
 from buffer import MessageBuffer
@@ -9,21 +9,21 @@ from buffer import MessageBuffer
 class FileHandler:
     @staticmethod
     def save_to_json(msg_buffer: MessageBuffer, filepath: str) -> None:
-        msg_list: List[Message] = []
+        msg_list = []
         if path.isfile(filepath):  # File found -> first read from existing file
             with open(filepath) as file:
                 msg_list = json.load(file)
 
-        msg_list.extend(msg_buffer.convert_memory_to_dict())
+        msg_list.extend(msg_buffer.to_dict())
 
         with open(filepath, 'w') as file:
-            json.dump(msg_list, file, indent=4, separators=(',', ': '))
+            json.dump(msg_list, file, indent=4)
 
     @staticmethod
-    def read_from_json(filepath: str) -> MessageBuffer: # TODO Change returned type for correct one.
+    def read_from_json(filepath: str) -> List[Dict]:
+        msg_list = []
         with open(filepath) as file:
-            msg_list: List[Message] = json.load(file) #TODO DICT type
-
+            msg_list = json.load(file)
 
         return msg_list
 
