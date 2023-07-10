aa3f8b20afc69f2954e68a623684ab753c0afad5
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-10 23:33:43 +0200

    feat(constants): MsgType added

--- constants.py
+++ constants.py
@@ -1,12 +1,25 @@
-from enum import StrEnum
+from enum import Enum
 
 
-class RotType(StrEnum):
+class RotType(str, Enum):
     ROT13 = 'ROT13'
     ROT47 = 'ROT47'
     NONE = 'NONE'
 
 
-class Status(StrEnum):
+class Status(str, Enum):
     ENCRYPTED = 'ENCRYPTED'
     DECRYPTED = 'DECRYPTED'
+
+
+class MsgType(str, Enum):
+    ENCODE = 'encode'
+    DECODE = 'decode'
+    SAVE = 'save'
+    DELETE = 'delete'
+
+
+# class Action(Enum):
+#     DECRYPTION = 0
+#     ENCRYPTION = 1
+
