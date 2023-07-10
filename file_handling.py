6f7371836d606dcc8c4c47ff4513669621667622
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-10 23:45:14 +0200

    refactor(FileHandler)!: saving method nmow accepts List[Dict] instead of MessageBuffer

--- file_handling.py
+++ file_handling.py
@@ -8,13 +8,13 @@ from buffer import MessageBuffer
 
 class FileHandler:
     @staticmethod
-    def save_to_json(msg_buffer: MessageBuffer, filepath: str) -> None:
+    def save_to_json(msg_buffer: List[Dict], filepath: str) -> None:
         msg_list = []
         if path.isfile(filepath):  # File found -> first read from existing file
             with open(filepath) as file:
                 msg_list = json.load(file)
 
-        msg_list.extend(msg_buffer.to_dict())
+        msg_list.extend(msg_buffer)
 
         with open(filepath, 'w') as file:
             json.dump(msg_list, file, indent=4)
