f2a97aa7b1970c7090ea28af5f9914ce82955389
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-07-10 23:33:04 +0200

    feat(buffer): can be iterated now

--- buffer.py
+++ buffer.py
@@ -11,6 +11,15 @@ class MessageBuffer:
     def __len__(self):
         return len(self.memory)
 
+    def __iter__(self):
+        return MessageBufferIter(self)
+
+    def __getitem__(self, idx):
+        return self.memory[idx]
+
+    def __setitem__(self, idx, value):
+        self.memory[idx] = value
+
     def display_all(self):
         for idx, msg in enumerate(self.memory, 1):
             print(f'{idx}. Status: {msg.status}, Encryption: {msg.rot_type}')
@@ -29,6 +38,20 @@ class MessageBuffer:
     def remove(self, idx_to_dlt) -> None:
         del self.memory[idx_to_dlt]
 
-    def to_dict(self):
-        return [msg.to_dict for msg in self.memory]
+    def to_dict(self) -> List[Dict]:
+        return [msg.to_dict() for msg in self.memory]
 
+
+class MessageBufferIter:
+    def __init__(self, buffer_class: MessageBuffer):
+        self._index = 0
+        self._size = len(buffer_class.memory)
+        self._memory = buffer_class.memory
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < self._size:
+            return self._memory[self._index]
+        raise StopIteration
