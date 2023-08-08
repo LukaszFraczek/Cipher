ad54ef70dd3ef5d49547a69619e4cbb9e2c00368
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-08-08 04:13:44 +0200

    refactor: changed enum classes to StrEnum inheritance

--- src/constants.py
+++ src/constants.py
@@ -1,19 +1,20 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class RotType(str, Enum):
+class RotType(StrEnum):
     ROT13 = 'ROT13'
     ROT47 = 'ROT47'
     NONE = 'NONE'
 
 
-class Status(str, Enum):
+class Status(StrEnum):
     ENCRYPTED = 'ENCRYPTED'
     DECRYPTED = 'DECRYPTED'
 
 
-class MsgType(str, Enum):
+class MsgType(StrEnum):
     ENCODE = 'encode'
     DECODE = 'decode'
     SAVE = 'save'
     DELETE = 'delete'
+    DISPLAY = 'display'
