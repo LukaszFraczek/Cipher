85c2881e6ffe7f9e99bf332efa86ca225fc7a5ad
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-08-08 20:01:03 +0200

    refactor: add missed typehints to file_handling class

--- src/file_handling.py
+++ src/file_handling.py
@@ -8,14 +8,13 @@ from src.menu import MenuMsg
 class FileHandler:
     @staticmethod
     def _load_msgs_from_file(file_path: str) -> List[Dict]:
-        msg_list = []
         with open(file_path) as file:
             msg_list = json.load(file)
-        return msg_list
+        return msg_list or []
 
     @staticmethod
     def _save_msgs_to_file(file_path: str, msg_list: List[Dict]) -> None:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             json.dump(msg_list, file, indent=4)
 
     @staticmethod
@@ -23,6 +22,7 @@ class FileHandler:
         existing_msgs = []
         if path.isfile(file_path):
             existing_msgs = FileHandler._load_msgs_from_file(file_path)
+
         try:
             merged_msgs = existing_msgs + new_msgs
             FileHandler._save_msgs_to_file(file_path, merged_msgs)
@@ -36,7 +36,4 @@ class FileHandler:
             msg_list = FileHandler._load_msgs_from_file(file_path)
         except FileNotFoundError:
             print(MenuMsg.FILE_NOT_FOUND)
-        return msg_list
-
-
-
+        return msg_list or []
