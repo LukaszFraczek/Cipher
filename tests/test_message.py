bd80934fd436accaf7cd1040f8f1c54d53ef0ed4
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-08-16 18:55:29 +0200

    feat: message tests finished

--- tests/test_message.py
+++ tests/test_message.py
@@ -5,13 +5,29 @@ from src.message import Message
 
 
 class TestMessage:
-    @pytest.mark.parametrize(
-        "msg",
-        [
-            Message("aaa", RotType.NONE, Status.DECRYPTED),
-            Message("bbb", RotType.ROT47, Status.ENCRYPTED),
-            Message("ccc", RotType.ROT13, Status.ENCRYPTED),
-        ],
+    @pytest.fixture(
+        params=[
+            (
+                Message("aaa", RotType.NONE, Status.DECRYPTED),
+                {"text": "aaa", "rot_type": "NONE", "status": "DECRYPTED"},
+            ),
+            (
+                Message("bbb", RotType.ROT47, Status.ENCRYPTED),
+                {"text": "bbb", "rot_type": "ROT47", "status": "ENCRYPTED"},
+            ),
+            (
+                Message("ccc", RotType.ROT13, Status.ENCRYPTED),
+                {"text": "ccc", "rot_type": "ROT13", "status": "ENCRYPTED"},
+            ),
+        ]
     )
-    def test_should_return_dict_of_message_values(self):
-        messages = []
+    def msg_dict_pair(self, request):
+        yield request.param[0], request.param[1]
+
+    def test_should_return_dict_of_message_values(self, msg_dict_pair):
+        msg, msg_dict = msg_dict_pair
+        assert msg.to_dict() == msg_dict
+
+    def test_should_return_message_constructed_from_dict(self, msg_dict_pair):
+        msg, msg_dict = msg_dict_pair
+        assert Message.from_dict(msg_dict) == msg
